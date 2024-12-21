2a9032763d1dc90c67727a1aeae862a3441138b3
himel1625 <himelmia1625@gmail.com>
2024-12-21 22:55:07 +0600

    implement authentication custom hooks

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,12 +1,36 @@
-import { createContext } from 'react';
-
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+} from 'firebase/auth';
+import { createContext, useEffect, useState } from 'react';
+import auth from '../Pages/Authentication/auth/auth';
 export const AuthContext = createContext(null);
+const googleProvider = new GoogleAuthProvider();
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+  // onAuthStateChange
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
 
   const AuthInfo = {
-    name: 'ph',
+    user,
+    setUser,
+    loading,
+    signInWithGoogle,
   };
   return (
     <div>
